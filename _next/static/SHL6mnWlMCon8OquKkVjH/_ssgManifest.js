d8787a83413a0da826316d223b2ac7ac6cde32dd
jcbritto <joao.britto@kyte.com.br>
2026-09-23 15:44:30 -0300

    livro 14 sobre montar acordes, audio dos livros 12, 13 e 14

--- _next/static/SHL6mnWlMCon8OquKkVjH/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Flivros\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  * O cache é VERSIONADO: bump CACHE_VERSION a cada deploy para invalidar o antigo.
  */
 
-const CACHE_VERSION = "perfumista-deck-v24";
+const CACHE_VERSION = "perfumista-deck-v25";
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
 // Caches que o USUÁRIO encheu de propósito, apertando "guardar no aparelho":
@@ -54,6 +54,7 @@ const PRECACHE_URLS = [
   "./livros/11-a-compra",
   "./livros/12-treinar-o-nariz",
   "./livros/13-moleculas-modernas",
+  "./livros/14-montar-acordes",
   "./manifest.webmanifest",
   "./photos/credits.json",
   "./icons/icon-192.png",
@@ -69,21 +70,21 @@ const PRECACHE_URLS = [
 const BUILD_ASSETS = [
   "./_next/static/chunks/01dkdyukbdpqq.js",
   "./_next/static/chunks/033x0xycwi-8w.js",
-  "./_next/static/chunks/08e4sqy61_n1_.js",
   "./_next/static/chunks/0cz1d0mv5g_q7.js",
   "./_next/static/chunks/0rmekjdoc_h_m.js",
   "./_next/static/chunks/0t43jt-g7nhl7.css",
+  "./_next/static/chunks/0y_zaa706vld1.js",
+  "./_next/static/chunks/0zdhpzdals7s2.js",
   "./_next/static/chunks/14mrh2-p_w84d.js",
   "./_next/static/chunks/1d-1fp49wc91u.js",
   "./_next/static/chunks/1ob81zqqu0j3s.js",
   "./_next/static/chunks/1s7svrwnaq1df.js",
   "./_next/static/chunks/27jktro2p5rq9.js",
+  "./_next/static/chunks/287lkqz8l5sd7.js",
   "./_next/static/chunks/2ghgxtl_j0i-h.js",
   "./_next/static/chunks/2j5mk6oa6vgc8.js",
   "./_next/static/chunks/2nvnl0j-ncw16.js",
   "./_next/static/chunks/2nykiepra7i1k.js",
-  "./_next/static/chunks/32m62u85ue-ul.js",
-  "./_next/static/chunks/37bkg01dertz9.js",
   "./_next/static/chunks/39txpi83_mtau.js",
   "./_next/static/chunks/turbopack-3sk924_ot2vst.js",
 ];
